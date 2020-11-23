9df037740f018e96a670fde7fe8e425e64e4330b
Gargi1510 <gargirai32@gmail.com>
2020-11-24 03:49:04 +0530

    login

--- app/src/main/java/com/example/myhotels/Util/User.java
+++ app/src/main/java/com/example/myhotels/Util/User.java
@@ -2,23 +2,61 @@ package com.example.myhotels.Util;
 
 import java.util.List;
 
+//public class User {
+//    private List<String> users;
+//    private List<String> passwords;
+//
+//    public List<String> getPasswords() {
+//        return passwords;
+//    }
+//
+//    public void setPasswords(List<String> passwords) {
+//        this.passwords = passwords;
+//    }
+//
+//    public List<String> getUsers() {
+//        return users;
+//    }
+//
+//    public void setUsers(List<String> users) {
+//        this.users = users;
+//    }
+//}
+
 public class User {
-    private List<String> users;
-    private List<String> passwords;
+    private String Name;
+    private String Password;
+    private String phone_number;
 
-    public List<String> getPasswords() {
-        return passwords;
+    //private String Notes;
+
+    public User() {
+
     }
 
-    public void setPasswords(List<String> passwords) {
-        this.passwords = passwords;
+    public User(String name, String password, String phonenumber) {
+        Name = name;
+        Password = password;
+        this.phone_number = phonenumber;
     }
 
-    public List<String> getUsers() {
-        return users;
+    public String getPhoneNumber() {
+        return phone_number;
     }
 
-    public void setUsers(List<String> users) {
-        this.users = users;
+    public void setPhoneNumber(String phonenumber) {
+        this.phone_number = phonenumber;
     }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) { this.Name = name; }       // added this. by shriyash
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {this.Password = password;   } // added this. by shriyash
 }
---
+++ app/src/main/java/com/example/myhotels/common/common.java
@@ -0,0 +1,8 @@
+package com.example.myhotels.common;
+
+import com.example.myhotels.Util.User;
+
+public class common {
+    public static User currentUser;
+
+}
